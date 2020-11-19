c3346677f200aa1281caeca0a97066852ccad9f5
HiroIshida <spitfire.docomo@gmail.com>
2020-11-20 00:16:37 +0900

    cleanup directory

--- python/wrapper.cpp
+++ python/wrapper.cpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/eigen.h>
-#include "../core.hpp"
+#include "../src/core.hpp"
 #include <Eigen/Dense>
 
 namespace py = pybind11;
