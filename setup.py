c158ebecfcba4ceb2a6afda7145d9f2affe46d27
HiroIshida <spitfire.docomo@gmail.com>
2020-11-19 17:17:39 +0900

    init

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+
+# see 
+# https://github.com/scikit-build/scikit-build-sample-projects/blob/master/projects/hello-pybind11/setup.py
+try:
+    from skbuild import setup
+except ImportError:
+    raise Exception
+
+setup(
+    name="pytinyfk",
+    version="1.2.3",
+    description="a minimal example package (with pybind11)",
+    author='Hirokazu Ishida',
+    license="MIT",
+    packages=["pytinyfk"],
+    package_dir={'': 'python'},
+    #cmake_install_dir='./uhouho_pytinyfk/', 
+    )
