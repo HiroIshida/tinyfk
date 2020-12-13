a0d455e602be42dd8e237adf131c07218e8861bc
HiroIshida <spitfire.docomo@gmail.com>
2020-12-14 04:27:30 +0900

    include model downloading in the test

--- download.sh
+++ download.sh
@@ -6,10 +6,10 @@ import gdown
 print(\"downloading robot models\")
 gdown.cached_download(
     url='https://drive.google.com/uc?id=1y7Jc3QoVW6J072CrSNupfKpyLp4NNxuH',
-    path=osp.join(\"./\", 'fetch_description.tar.gz'),
+    path=osp.join(\"./data\", 'fetch_description.tar.gz'),
     md5='fbe29ab5f3d029d165a625175b43a265',
     postprocess=gdown.extractall,
     quiet=True,
 )
 "
-rm fetch_description.tar.gz
+rm ./data/fetch_description.tar.gz
