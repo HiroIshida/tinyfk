bff035c9b9c4fec9b133f58c6b0dd442a91adfeb
HiroIshida <spitfire.docomo@gmail.com>
2020-12-14 21:02:13 +0900

    add basic test

---
+++ python/tests/test_tinyfk.py
@@ -0,0 +1,17 @@
+import os
+import json
+import unittest
+import numpy as np
+import tinyfk
+
+here_full_filepath = os.path.join(os.getcwd(), __file__)
+here_full_dirpath = os.path.dirname(here_full_filepath)
+project_base_path = os.path.join(here_full_dirpath, "..", "..")
+
+urdf_model_path = os.path.join(project_base_path, "data", "fetch_description", "fetch.urdf")
+test_data_path = os.path.join(project_base_path, "test", "test_data.json")
+
+with open(test_data_path, 'r') as f:
+    test_data = json.load(f)
+
+fksolver = tinyfk.RobotModel(urdf_model_path)
