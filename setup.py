e5e3b9fa74cee3e6653b91f1e0af202adf06e2fe
HiroIshida <spitfire.docomo@gmail.com>
2020-12-18 23:50:35 +0900

    0.2.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name="tinyfk",
-    version="0.2.2",
+    version="0.2.3",
     description="a fast kinematics solver",
     author='Hirokazu Ishida',
     license="MIT",
