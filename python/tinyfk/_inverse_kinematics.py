a401e21671160488dbbe2d568c88584c8ba3472f
HiroIshida <spitfire.docomo@gmail.com>
2020-12-29 09:40:05 +0900

    split ik related method from __init__

---
+++ python/tinyfk/_inverse_kinematics.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def solve_inverse_kinematics(self, target_pose, init_angle_vector, elink_id, joint_ids,
+        with_rot=False, with_base=False, option=None, ignore_fail=False):
+
+    if option is None:
+        option = {"maxitr": 200, "ftol": 1e-4, "sr_weight":1.0}
+
+    target_pose = np.array(target_pose)
+    with_rot = len(target_pose) == 6
+    n_dof = len(joint_ids) + (3 if with_base else 0)
+    assert len(init_angle_vector) == n_dof
+
+    pose_dim = (6 if with_rot else 3)
+    assert len(target_pose) == pose_dim
+
+    angle_vector = init_angle_vector
+    for i in range(option["maxitr"]):
+        P, J = self._robot.solve_forward_kinematics(
+                [angle_vector], [elink_id], joint_ids, with_rot, with_base, True)
+        pose_diff = target_pose - P[0]
+        J_sharp = J.T.dot(np.linalg.inv(J.dot(J.T) + option["sr_weight"])) # singular-robust inverse
+        angle_vector = angle_vector + J_sharp.dot(pose_diff)
+        if np.linalg.norm(pose_diff) < option["ftol"]:
+            return angle_vector
+
+    if ignore_fail:
+        return angle_vector
+
+    raise IKFail
+
+"""
+def solve_multi_endeffector_inverse_kinematics(self,
+        target_pose_list, init_angle_vector, elink_ids, joint_ids,
+        with_rot=False, with_base=False, option=None, ignore_fail=False):
+
+    if option is None:
+        option = {"maxitr": 200, "ftol": 1e-4, "sr_weight":1.0}
+"""
