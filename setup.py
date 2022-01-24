e83d45e584e0bb446dae3a7d4a1d0f10d00a53b9
HiroIshida <spitfire.docomo@gmail.com>
2022-01-25 05:59:50 +0900

    Bump to 0.4.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name="tinyfk",
-    version="0.4.4",
+    version="0.4.5",
     description="a fast kinematics solver",
     author='Hirokazu Ishida',
     license="MIT",
