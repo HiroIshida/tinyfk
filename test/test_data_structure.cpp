f39f0f370e309f491a1f385c22424f128912ff48
HiroIshida <spitfire.docomo@gmail.com>
2022-05-27 06:38:57 +0900

    Use gtest for test_data_structure

--- test/test_data_structure.cpp
+++ test/test_data_structure.cpp
@@ -1,19 +1,24 @@
-#include <cassert>
 #include <data_structure.hpp>
+#include <gtest/gtest.h>
 
-int main() {
+TEST(DataStructureTest, SizedStack) {
   tinyfk::SizedStack<int> s(100);
   s.top();
   assert(s.empty());
   s.push(1);
   s.push(2);
-  assert(s.size() == 2);
-  assert(s.top() == 2);
+  EXPECT_EQ(s.size(), 2);
+  EXPECT_EQ(s.top(), 2);
   s.pop();
   s.pop();
-  assert(s.empty());
+  EXPECT_TRUE(s.empty());
   s.reset();
   s.push(100);
   s.push(200);
-  assert(s.top() == 200);
+  EXPECT_EQ(s.top(), 200);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
