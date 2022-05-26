70b5a71795e9306c4d6f3539ffd7d6d2c900bcfc
HiroIshida <spitfire.docomo@gmail.com>
2022-05-27 07:34:37 +0900

    Use gtest for kinematics test

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #include "tinyfk.hpp"
 #include <cmath>
 #include <fstream>
+#include <gtest/gtest.h>
 #include <nlohmann/json.hpp>
 #include <stdexcept>
 #include <string>
@@ -12,7 +13,7 @@ using namespace tinyfk;
 
 bool isNear(double x, double y) { return (abs(x - y) < 1e-5); }
 
-int main() {
+TEST(KINEMATICS, AllTest) {
   // loading test data
   ifstream test_data("../test/test_data.json");
   nlohmann::json js;
@@ -42,15 +43,10 @@ int main() {
     std::array<double, 3> pos = {0.1, 0.1, 0.1};
     std::array<double, 3> rot = {0.3, 0.2, 0.1};
     int parent_link_id = robot.get_link_ids(strvec)[0];
-    try {
-      robot.add_new_link("mylink", parent_link_id, pos, rot);
-      std::cout << "[FAIL] must raise exception (add_new_link)" << std::endl;
-      return -1;
-    } catch (const std::exception &e) {
-      std::cout << "[PASS] successfully raise exception in add_new_link"
-                << std::endl;
-    }
+    EXPECT_THROW(robot.add_new_link("mylink", parent_link_id, pos, rot),
+                 std::exception);
   }
+
   { // check if rptable is propery updated
     std::vector<std::string> joint_names = {"torso_lift_joint",
                                             "r_wrist_flex_joint"};
@@ -58,34 +54,10 @@ int main() {
                                            "fl_caster_rotation_link"};
     auto joint_ids = robot.get_joint_ids(joint_names);
     auto link_ids = robot.get_link_ids(link_names);
-    if (!robot.rptable_.isRelevant(joint_ids[0], link_ids[0])) {
-      std::cout << "[FAIL] torso_lift_joint must move mylink" << std::endl;
-      return -1;
-    } else {
-      std::cout << "[PASS] rptable" << std::endl;
-    }
-    if (robot.rptable_.isRelevant(joint_ids[1], link_ids[0])) {
-      std::cout << "[FAIL] r_wrist_flex_joint must not move mylink"
-                << std::endl;
-      return -1;
-    } else {
-      std::cout << "[PASS] rptable" << std::endl;
-    }
-    if (!robot.rptable_.isRelevant(joint_ids[0], link_ids[1])) {
-      std::cout << "[FAIL] torso_lift_joint must move head_pan_link"
-                << std::endl;
-      return -1;
-    } else {
-      std::cout << "[PASS] rptable" << std::endl;
-    }
-    if (robot.rptable_.isRelevant(joint_ids[0], link_ids[2])) {
-      std::cout
-          << "[FAIL] torso_lift_joint must not move fl_caster_rotation_link"
-          << std::endl;
-      return -1;
-    } else {
-      std::cout << "[PASS] rptable" << std::endl;
-    }
+    EXPECT_TRUE(robot.rptable_.isRelevant(joint_ids[0], link_ids[0]));
+    EXPECT_FALSE(robot.rptable_.isRelevant(joint_ids[1], link_ids[0]));
+    EXPECT_TRUE(robot.rptable_.isRelevant(joint_ids[0], link_ids[1]));
+    EXPECT_FALSE(robot.rptable_.isRelevant(joint_ids[0], link_ids[2]));
   }
 
   auto joint_ids = robot.get_joint_ids(joint_names);
@@ -96,37 +68,22 @@ int main() {
   }
   robot.set_base_pose(angle_vector[n_joints + 0], angle_vector[n_joints + 1],
                       angle_vector[n_joints + 2]);
+
   bool base_also = true;
   urdf::Pose pose, pose_naive;
   for (size_t i = 0; i < n_links; i++) {
     int link_id = link_ids[i];
-    robot.get_link_pose(link_id, pose, base_also);
 
-    if (!isNear(pose.position.x, pose_list[i][0]) ||
-        !isNear(pose.position.y, pose_list[i][1]) ||
-        !isNear(pose.position.z, pose_list[i][2])) {
-      std::cout << "[FAIL] position of " << link_names[i] << " does not match"
-                << std::endl;
-      std::cout << "expected : " << pose_list[i][0] << " " << pose_list[i][1]
-                << " " << pose_list[i][2] << std::endl;
-      std::cout << "computed : " << pose.position.x << " " << pose.position.y
-                << " " << pose.position.z << std::endl;
-      return -1;
-    }
+    robot.get_link_pose(link_id, pose, base_also);
+    EXPECT_TRUE(isNear(pose.position.x, pose_list[i][0]));
+    EXPECT_TRUE(isNear(pose.position.y, pose_list[i][1]));
+    EXPECT_TRUE(isNear(pose.position.z, pose_list[i][2]));
 
     auto rpy = pose.rotation.getRPY();
-    if (
-        // NOTE that rpy of URDFdom is (r, p, y) order, but (y, p, r) in skrobot
-        !isNear(rpy.z, pose_list[i][3]) || !isNear(rpy.y, pose_list[i][4]) ||
-        !isNear(rpy.x, pose_list[i][5])) {
-      std::cout << "[FAIL] rpy of " << link_names[i] << " does not match"
-                << std::endl;
-      return -1;
-    }
-    std::cout << "[PASS] comparison with ground truth of link " << link_names[i]
-              << std::endl;
+    EXPECT_TRUE(isNear(rpy.z, pose_list[i][3]));
+    EXPECT_TRUE(isNear(rpy.y, pose_list[i][4]));
+    EXPECT_TRUE(isNear(rpy.x, pose_list[i][5]));
   }
-  std::cout << "[PASS] get_link_point_withcache" << std::endl;
 
   // Now we comapre jacobian computed by finite diff with the analytical one
   for (size_t i = 0; i < n_joints; i++) {
@@ -149,15 +106,11 @@ int main() {
 
     bool jacobian_equal =
         (J_analytical_block - J_numerical).array().abs().maxCoeff() < 1e-5;
-    if (!jacobian_equal) {
-      std::cout << "analytical :\n" << J_analytical_block << std::endl;
-      std::cout << "numerical :\n" << J_numerical << std::endl;
-      std::cout << "[FAIL] jacobains of " << link_names[i] << "mismatch"
-                << std::endl;
-      return -1;
-    }
-    std::cout << "[PASS] jacobian match for link : " << link_names[i]
-              << std::endl;
+    EXPECT_TRUE(jacobian_equal);
   }
-  std::cout << "[PASS] fk solve compare" << std::endl;
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
