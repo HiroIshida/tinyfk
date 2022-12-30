9c8e1d518cccea37d7a78ff492e54b57c55b1643
HiroIshida <spitfire.docomo@gmail.com>
2022-12-31 02:59:53 +0900

    refactor: remove no longer used functions

--- include/tinyfk.hpp
+++ include/tinyfk.hpp
@@ -23,67 +23,7 @@ tinyfk: https://github.com/HiroIshida/tinyfk
 
 namespace tinyfk {
 
-using MatrixXdC =
-    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor>;
-using MatrixXdR =
-    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
 using AngleLimit = std::pair<double, double>;
-
-// TODO templatize
-// an util data structure to handle matrix.
-struct SlicedMatrix // coll major (same as eigen)
-{
-  double *data_; // beginning of the block matrix
-  int i_begin_;
-  int j_begin_;
-
-  int n_block_;
-  int m_block_;
-
-  int n_whole_;
-  int m_whole_;
-
-  SlicedMatrix(Eigen::MatrixXd &mat, int i_begin, int j_begin, int n, int m)
-      : data_(mat.data()), i_begin_(i_begin), j_begin_(j_begin), n_block_(n),
-        m_block_(m), n_whole_(mat.rows()), m_whole_(mat.cols()) {}
-
-  SlicedMatrix(Eigen::MatrixXd &mat)
-      : data_(mat.data()), i_begin_(0), j_begin_(0), n_block_(mat.rows()),
-        m_block_(mat.cols()), n_whole_(n_block_), m_whole_(m_block_) {}
-
-  SlicedMatrix(double *data, int i_begin, int j_begin, int n, int m,
-               int n_whole, int m_whole)
-      : data_(data), i_begin_(i_begin), j_begin_(j_begin), n_block_(n),
-        m_block_(m), n_whole_(n_whole), m_whole_(m_whole) {}
-
-  inline int rows() const { return n_block_; }
-
-  inline int cols() const { return m_block_; }
-
-  inline int get_idx(int i, int j) const {
-    assert(i < n_whole_ && "out of index");
-    assert(j < m_whole_ && "out of index");
-
-    int idx = n_whole_ * (j + j_begin_) + (i + i_begin_);
-    return idx;
-  }
-
-  SlicedMatrix block(int i, int j, int n, int m) const {
-    SlicedMatrix mat = {data_, i_begin_ + i, j_begin_ + j, n,
-                        m,     n_whole_,     m_whole_};
-    return mat;
-  }
-
-  SlicedMatrix slice(int i) const { // we consider matrix is coll major.
-    return this->block(0, i, n_block_, 1);
-  }
-
-  double &operator()(int i, int j) const { return data_[this->get_idx(i, j)]; }
-
-  double &operator[](int i) const { // access to sliced matrix
-    return data_[this->get_idx(i, 0)];
-  }
-};
 
 struct RelevancePredicateTable {
   std::vector<std::vector<bool>> table_;
@@ -188,26 +128,6 @@ private:
 class CacheUtilizedRobotModel : public RobotModelBase {
 public:
   using RobotModelBase::RobotModelBase;
-
-  void solve_forward_kinematics(int elink_id,
-                                const std::vector<size_t> &joint_ids,
-                                bool with_rot, bool with_base,
-                                SlicedMatrix &pose_arr,
-                                SlicedMatrix &jacobian) const;
-
-  void solve_batch_forward_kinematics(std::vector<size_t> elink_ids,
-                                      const std::vector<size_t> &joint_ids,
-                                      bool with_rot, bool with_base,
-                                      SlicedMatrix &pose_arr,
-                                      SlicedMatrix &jacobian_arr) const;
-
-  // perfromance of returning array of eigen is actually almost same as pass by
-  // reference thanks to the compiler's optimization
-  std::array<Eigen::MatrixXd, 2> get_jacobians_withcache(
-      const std::vector<size_t> &elink_ids,
-      const std::vector<size_t> &joint_ids,
-      bool rpyalso = false, // only point jacobian is computed by default
-      bool basealso = false) const;
 
   void get_link_pose(size_t link_id, urdf::Pose &out_tf_root_to_ef,
                      bool usebase) const;
