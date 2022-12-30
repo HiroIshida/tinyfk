3dc720afc7656f927c4a916493136b08fddfaa5d
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2022-12-31 05:23:51 +0900

    feat: compute inter link sqdist (#60)

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -9,6 +9,7 @@ tinyfk: https://github.com/HiroIshida/tinyfk
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <utility>
 
 namespace py = pybind11;
 using namespace tinyfk;
@@ -114,6 +115,61 @@ public:
     robot_model_.add_new_link(link_name, parent_id, position, rotation);
   }
 
+  std::pair<Eigen::VectorXd, Eigen::MatrixXd>
+  compute_inter_link_squared_dists(const std::vector<std::vector<double>> &qs,
+                                   const std::vector<size_t> &link_ids1,
+                                   const std::vector<size_t> &link_ids2,
+                                   const std::vector<size_t> &joint_ids,
+                                   bool with_base, bool with_grads,
+                                   bool use_cache) {
+    const size_t n_wp = qs.size();
+    const size_t n_joint = joint_ids.size();
+    const size_t n_check = link_ids1.size();
+
+    Eigen::VectorXd sqdists = Eigen::VectorXd::Zero(n_check * n_wp);
+    Eigen::MatrixXd grads = with_grads
+                                ? Eigen::MatrixXd::Zero(n_check * n_wp, n_joint)
+                                : Eigen::MatrixXd::Zero(1, 1);
+
+    for (size_t i = 0; i < n_wp; i++) {
+      robot_model_._set_joint_angles(joint_ids, qs[i]);
+      if (with_base) {
+        double x = qs[i][n_joint + 0];
+        double y = qs[i][n_joint + 1];
+        double theta = qs[i][n_joint + 2];
+        robot_model_._set_base_pose(x, y, theta);
+      }
+      if (!use_cache) {
+        robot_model_.clear_cache();
+      }
+    }
+
+    for (size_t i = 0; i < n_wp; ++i) {
+      for (size_t j = 0; j < n_check; ++j) {
+        const size_t head = i * n_check + j;
+        urdf::Pose pose1, pose2;
+        robot_model_.get_link_pose(link_ids1[j], pose1, with_base);
+        robot_model_.get_link_pose(link_ids2[j], pose2, with_base);
+        const auto diff = pose1.position - pose2.position;
+        Eigen::Vector3d diff_vec;
+        diff_vec << diff.x, diff.y, diff.z;
+        const double sqdist = diff_vec.squaredNorm();
+        sqdists(head) = sqdist;
+
+        if (with_grads) {
+          const auto &&jac1 = robot_model_.get_jacobian(link_ids1[j], joint_ids,
+                                                        false, with_base);
+          const auto &&jac2 = robot_model_.get_jacobian(link_ids2[j], joint_ids,
+                                                        false, with_base);
+          const auto grad = 2 * (jac1 - jac2).transpose() * diff_vec;
+          std::cout << grad << std::endl;
+          grads.block(head, 0, 1, n_joint) = grad.transpose();
+        }
+      }
+    }
+    return std::pair<Eigen::VectorXd, Eigen::MatrixXd>{sqdists, grads};
+  }
+
   void clear_cache() { robot_model_.clear_cache(); }
 };
 
@@ -129,5 +185,7 @@ PYBIND11_MODULE(_tinyfk, m) {
       .def("set_base_pose", &RobotModelPyWrapper::set_base_pose)
       .def("get_link_ids", &RobotModelPyWrapper::get_link_ids)
       .def("add_new_link", &RobotModelPyWrapper::add_new_link)
+      .def("compute_inter_link_squared_dists",
+           &RobotModelPyWrapper::compute_inter_link_squared_dists)
       .def("clear_cache", &RobotModelPyWrapper::clear_cache);
 }
