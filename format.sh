2d6b19f40b243879bd0afa456b31d3126300ea6a
HiroIshida <spitfire.docomo@gmail.com>
2022-12-31 09:25:29 +0900

    chore: add clang-format in formatter

--- format.sh
+++ format.sh
@@ -3,3 +3,5 @@ find python -name "*py"|xargs python3 -m autoflake -i --remove-all-unused-import
 python3 -m isort python/
 python3 -m black python/
 python3 -m flake8 python/
+find src/ include/ test/ bench/ -name "*.cpp"|xargs clang-format -i
+find src/ include/ test/ bench/ -name "*.hpp"|xargs clang-format -i
