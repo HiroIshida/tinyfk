f18ea423e9edd38c8ffab047bd28e396513c55a9
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-07-11 15:03:37 +0900

    0.6.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name="tinyfk",
-    version="0.6.6",
+    version="0.6.7",
     description="a fast kinematics solver",
     author='Hirokazu Ishida',
     license="MIT",
